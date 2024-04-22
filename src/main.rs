aa716d96d081c13b2df581360d74afdf5ffc2b7a
rzapriono <apriono.reza@gmail.com>
2024-04-22 18:43:56 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
 
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
 
         println!("In Reza Apriono's Computer [2206827945]. Message received: {:?}", message);
         Ok(())
